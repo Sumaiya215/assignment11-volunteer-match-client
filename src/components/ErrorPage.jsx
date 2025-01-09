839cc4caa207005386175b751dbf32b3039b97fb
Sumaiya215 <sumaiyacse15>
2025-01-09 15:09:05 -0800

    added a new page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import errorImage from '../assets/404.webp'
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+    const handleError = () => {
+        navigate(-1)
+    }
+
+    const handleError2 = () => {
+        navigate('/')
+    }
     return (
-        <div>
-            not found
+        <div className='w-1/2 mx-auto  mt-20'>
+            <div className='text-center mb-8'>
+                <img className='w-1/2 mx-auto ' src={errorImage} alt="" />
+            </div>
+            <div className='text-center'>
+                <button onClick={handleError} className='btn btn-outline btn-primary mr-4'>Go Back</button>
+                <button onClick={handleError2} className='btn btn-outline btn-primary'>Back to Home</button>
+            </div>
+
+
         </div>
     );
 };
